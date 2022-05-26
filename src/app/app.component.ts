295f8f49bd6155b449c4cdf29d784dc0977c71d6
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-26 22:58:17 +0530

    generate random text and define onInput function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'typing-challenge';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    console.log(target.value);
+    this.enteredText = target.value;
+  }
+  
+
 }
